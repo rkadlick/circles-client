7452bdcc26296fbfecc6568970a0d45634b62361
rkadlick <27776470+rkadlick@users.noreply.github.com>
2024-11-12 13:24:53 -0500

    add color scheme

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux';
 import {store} from './redux/store';
+import './colorVariables.css';
 
 createRoot(document.getElementById('root')!).render(
   //<StrictMode>
